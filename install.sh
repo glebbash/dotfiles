5e9ae3bbb5d50613ebda12c4bb6477d7b624d4ed
glebbash <glebbash@gmail.com>
2023-05-03 14:24:23 +0000

    fix: update function syntax

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
-function main() {
+main() {
     aliases_setup || (echo "aliases_setup job failed and exited" && exit 1)
     npm_setup || (echo "npm_setup job failed and exited" && exit 1)
     git_setup || echo "git_setup job failed and continued"
 }
 
-function aliases_setup() {
+aliases_setup() {
     echo "Setting up bash aliases"
 
     cp "${HOME}/.dotfiles/.bash_aliases" "${HOME}/.bash_aliases"
 }
 
-function npm_setup() {
+npm_setup() {
     echo "Setting up NPM"
 
     if [ -n "${EXTENDA_NEXUS_TOKEN:-}" ]; then
@@ -19,7 +19,7 @@ function npm_setup() {
     fi
 }
 
-function git_setup() {
+git_setup() {
     echo "Setting up Git"
 
     git config --global push.autoSetupRemote true
