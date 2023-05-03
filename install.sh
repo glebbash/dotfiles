671f363389cfba80e5e367b275640c230d0244c1
glebbash <glebbash@gmail.com>
2023-05-03 16:44:38 +0300

    chore: format install.sh

--- install.sh
+++ install.sh
@@ -13,21 +13,21 @@ function npm_setup() {
 }
 
 function git_setup() {
-  echo "Setting up Git"
+    echo "Setting up Git"
 
-  git config --global push.autoSetupRemote true
+    git config --global push.autoSetupRemote true
 
-  echo "Git: Use rebase to pull"
-  git config --global pull.rebase true
+    echo "Git: Use rebase to pull"
+    git config --global pull.rebase true
 
-  if [ -n "${GPG_PRIVATE_KEY_BASE64:-}" ]; then
-    echo "Git: Installing GPG key"
-    gpg --verbose --batch --import <(echo "${GPG_PRIVATE_KEY_BASE64}" | base64 -d)
-    echo 'pinentry-mode loopback' >> ~/.gnupg/gpg.conf
-    git config --global user.signingkey "${GPG_SIGNING_KEY}"
-    git config --global commit.gpgsign true
-    git config --global tag.gpgsign true
-  fi
+    if [ -n "${GPG_PRIVATE_KEY_BASE64:-}" ]; then
+        echo "Git: Installing GPG key"
+        gpg --verbose --batch --import <(echo "${GPG_PRIVATE_KEY_BASE64}" | base64 -d)
+        echo 'pinentry-mode loopback' >> ~/.gnupg/gpg.conf
+        git config --global user.signingkey "${GPG_SIGNING_KEY}"
+        git config --global commit.gpgsign true
+        git config --global tag.gpgsign true
+    fi
 }
 
 main "$@"
